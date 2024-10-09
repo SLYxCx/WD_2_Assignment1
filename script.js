7a3ec706db733055116451ab99a0b4a33518d46d
SLYxCx <99172731+SLYxCx@users.noreply.github.com>
2024-10-08 19:32:43 -0600

    Index-ColourUpdate / script-ToDoList
    Changed colours per tab in Index.html
    
    Added To-Do list in script.js

--- script.js
+++ script.js
@@ -18,3 +18,31 @@ function showTab(tabId) {
         }
 }
 
+// Submit Button 
+// function submitBtn {
+//     console.log('submit')
+// }
+
+// Convert Weight
+// function convertWeight(submit) {
+//     console.log('weight') // placeholder
+//     /*
+//     1. Make the Submit button work
+//     2. Input to take data to an array (comma separated values)
+//     3. For each → convert values
+//         Based on radio selection
+//     4. Output (console.log to test it's working)
+//     5. Add element to the page to display outputs
+//     */
+// };
+
+// Convert Distance
+// function convertDistance {
+//     console.log('distance')
+// };
+
+// Convert Temperature
+// function convertTemperature {
+//     console.log('temperature')
+// };
+
