318750e0f0735e9cd772c366d4f711f41c110d8d
SLYxCx <99172731+SLYxCx@users.noreply.github.com>
2024-10-09 01:20:05 -0600

    Convert Distance Functionality
    Added Distance conversion functionality
    with mock data
    
    Added display functionality (currently just happens with the mock data; but could/should (maybe) be added to 'Button' functionality)
    
    Added 'results' section to Index.html

--- script.js
+++ script.js
@@ -38,9 +38,31 @@ function showTab(tabId) {
 // };
 
 // Convert Distance
-// function convertDistance {
-//     console.log('distance')
-// };
+// Mock data:
+var userInput = [26, 10, 50, -40]; // Seems simplest to make the user input an array of numbers; even if it's one value
+
+var isKmToMi = true; // if set to km → mi (if mi → km set to false) ("is Kilometers To Miles")
+
+// Formulas
+const KilometersToMiles = input => input / 1.6;
+const MilesToKilometers = input => input * 1.6;
+
+// Conversion: takes in user input array → Goes through each index, converts them based on if isKmToMi is 'true' or 'false'
+//      (map instead of forEach - because map is "ideal when you need to transform data and get a new array as the output")
+const convertDistance = (userInput) => {
+    var convertedArray = [];
+    if (isKmToMi) {
+        convertedArray = userInput.map(element => KilometersToMiles(element));
+    } else {
+        convertedArray = userInput.map(element => MilesToKilometers(element));
+    }
+    return convertedArray;
+};
+
+const convertedData = document.createElement("p");
+convertedData.innerHTML = convertDistance(userInput);
+document.getElementById('results').appendChild(convertedData);
+console.log(convertDistance(userInput));
 
 // Convert Temperature
 // function convertTemperature {
